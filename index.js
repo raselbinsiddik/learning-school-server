13d25764382b816628dcdd2f173ef0d3a7829165
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-06-12 00:44:06 +0600

    veryfyadmin crete

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/users', async (req, res) => {
+        app.get('/users',verifyJWT,verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
